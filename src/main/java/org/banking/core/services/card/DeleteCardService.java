aa113c2735de769b815d5bca76cbc7001fb0879c
VladKuli <goldydefault@gmail.com>
2025-05-24 20:19:02 +0300

    Refactoring Card logic (using more modern principle, wire dependencies through the constructor)

--- src/main/java/org/banking/core/services/card/DeleteCardService.java
+++ src/main/java/org/banking/core/services/card/DeleteCardService.java
@@ -1,37 +1,41 @@
 package org.banking.core.services.card;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.banking.core.database.JpaCardRepository;
 import org.banking.core.domain.Card;
 import org.banking.core.request.card.DeleteCardRequest;
 import org.banking.core.response.card.DeleteCardResponse;
-import org.banking.core.services.bankAccount.GetCurrentBankAccountService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class DeleteCardService {
 
-    @Autowired
-    private JpaCardRepository jpaCardRepository;
+    private final JpaCardRepository jpaCardRepository;
 
-    private static final Logger logger = LoggerFactory.getLogger(DeleteCardService.class);
 
     public DeleteCardResponse execute(DeleteCardRequest request) {
 
-        logger.info("Deleting card with request: {}",request.getCardNumber());
+        log.info("Deleting card with request: {}",request.getCardNumber());
          jpaCardRepository.deleteByCardNumber(request.getCardNumber());
-        logger.info("Is Card deleted check from this request: {}", request.getCardNumber());
+
+        log.info("Is Card deleted check from this request: {}", request.getCardNumber());
         Optional<Card> card = jpaCardRepository.existsByCardNumber(request.getCardNumber());
-        logger.debug("Checking is CardPresent: {}",card);
+
+        log.debug("Checking is CardPresent: {}",card);
         if (card.isPresent()) {
-            logger.warn("Error: Card is exist: {}",card);
+
+            log.warn("Error: Card is exist: {}",card);
             return new DeleteCardResponse(false);
+
         } else {
-            logger.warn("Card successfully deleted");
+
+            log.warn("Card successfully deleted");
             return new DeleteCardResponse(true);
         }
     }
