6038131ed4c09df1b1d5866d93cacbcaafb86fa1
VladKuli <goldydefault@gmail.com>
2025-05-25 13:05:21 +0300

    Adding validation

--- src/main/java/org/banking/core/dto/user/UserDTO.java
+++ src/main/java/org/banking/core/dto/user/UserDTO.java
@@ -5,12 +5,23 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class UserDTO {
 
+    @NotNull(message = "Personal code cannot be null")
+    @NotEmpty(message = "Personal code cannot be empty")
     private String personalCode;
+
+    @NotNull(message = "Password cannot be null")
+    @NotEmpty(message = "Password cannot be empty")
     private String password;
+
+    @NotNull(message = "Role cannot be null")
+    @NotEmpty(message = "Role cannot be empty")
     private String role;
 }
